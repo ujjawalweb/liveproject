abb7719a0d03eca1ab3b798d5b21627a0e206d3f
ujjawalweb <85484866+ujjawalweb@users.noreply.github.com>
2022-03-13 17:44:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
   <>
