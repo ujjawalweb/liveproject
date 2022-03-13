f76423eeba77ce6c4f6faf5fcf0ba61f3076268f
ujjawalweb <85484866+ujjawalweb@users.noreply.github.com>
2022-03-13 22:55:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.render(
   <>
-   <BrowserRouter basename="/Home">
+   <BrowserRouter basename="/App">
     <App />
  </BrowserRouter>
   </>,
